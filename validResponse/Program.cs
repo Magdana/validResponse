8fa9ebdc2da7f3bf92ba0b106dc1e92b7eaabca7
Magda <magdagvirjishvili@gmail.com>
2023-07-12 20:26:41 +0400

    magdas commit

---
+++ validResponse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+
+        do
+        {
+            Console.WriteLine("Your response must be 'yes' or 'no'");
+            Console.WriteLine("Please enter the text:");
+            input = Console.ReadLine().ToLower();
+        }
+        while (input != "yes" && input != "no");
+
+        Console.WriteLine("You entered: " + input);
+        Console.ReadLine();
+    }
+}
